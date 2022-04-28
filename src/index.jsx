22578ceb3073b8f9376912d6d067f5f4d37ad186
christopheralphonse <christopheralphonse96@gmail.com>
2022-04-28 15:22:43 -0400

    :smile:  added react-router-dom and render method

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
-<BrowserRouter>
-  <App />
-</BrowserRouter>;
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
