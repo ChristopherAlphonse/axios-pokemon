84167ff5e8cb1645b74e0c5449f1e8a3db24fb4b
christopheralphonse <christopheralphonse96@gmail.com>
2022-04-28 18:45:53 -0400

    :smile: added NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <h1>Bookkeeper</h1>
       <nav>
-        <Link to="/invoices">Book 1</Link> | &nbsp;
-        <Link to="/expenses">Book 2</Link>
+        <NavLink to="/invoices">Book 1</NavLink> | &nbsp;
+        <NavLink to="/expenses">Book 2</NavLink>
       </nav>
     </div>
   );
