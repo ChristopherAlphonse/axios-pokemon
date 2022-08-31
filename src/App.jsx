49dd57a5a0ecca30a1e9a2bf53b6d81fabc689b1
Christopher Alphonse <christopheralphonse96.com>
2022-08-30 22:30:40 -0400

    messing arround

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 const App = () => {
   const [pokeData, setPokeData] = useState([]);
+
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=8")
       .then((response) => {
         setPokeData(response.data.results);
+        console.log(response.data.results);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -14,10 +18,10 @@ const App = () => {
     <main className="container">
       <h1> Pokemon API </h1>
 
-      <p>*displaying 807 pokemon names</p>
+      <p>*displaying 8 pokemon names</p>
       <ul>
         {pokeData.map((apidata) => (
-          <li key={apidata.url}>{apidata.name} </li>
+          <li key={apidata.url}>{apidata.name[0] + " " + apidata.name[6]} </li>
         ))}
       </ul>
     </main>
